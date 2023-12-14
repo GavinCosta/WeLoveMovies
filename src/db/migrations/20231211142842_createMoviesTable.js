a81369cefb26e172f26783c0db7dc037c934e18c
Gavin Costa <gavin.michael.costa@gmail.com>
2023-12-14 15:19:34 -0500

    cleaned up migrations

--- src/db/migrations/20231211142842_createMoviesTable.js
+++ src/db/migrations/20231211142842_createMoviesTable.js
@@ -2,15 +2,15 @@
 exports.up = function(knex) {
   return knex.schema.createTable('movies', (table) => {
     table.increments('movie_id').primary();
-    table.string('title')
-    table.integer('runtime_in_minutes')
-    table.string('rating')
-    table.text('description')
-    table.string('image_url')
-    table.timestamps(true, true)
-  })
+    table.string('title').notNullable();
+    table.integer('runtime_in_minutes').notNullable();
+    table.string('rating');
+    table.text('description');
+    table.string('image_url').notNullable();
+    table.timestamps(true, true);
+  });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('movies')
+  return knex.schema.dropTable('movies');
 };
--- src/db/migrations/20231211142902_createTheatersTable.js
+++ src/db/migrations/20231211142902_createTheatersTable.js
@@ -2,16 +2,16 @@
 exports.up = function(knex) {
   return knex.schema.createTable('theaters', (table) => {
     table.increments('theater_id').primary();
-    table.string('name')
-    table.string('address_line_1')
-    table.string('address_line_2')
-    table.string('city')
-    table.string('state')
-    table.string('zip')
-    table.timestamps(true, true)
-  })
+    table.string('name').notNullable();
+    table.string('address_line_1').notNullable();
+    table.string('address_line_2');
+    table.string('city').notNullable();
+    table.string('state').notNullable();
+    table.string('zip').notNullable();
+    table.timestamps(true, true);
+  });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('theaters')
+  return knex.schema.dropTable('theaters');
 };
